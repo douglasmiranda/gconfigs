a3f04be3bec649bee420396a84a7b8d1c6c92fd6
Douglas Miranda <douglasmirandasilva@gmail.com>
2018-04-10 01:51:26 -0400

    Python 3 since the setup.py!

--- setup.py
+++ setup.py
@@ -4,31 +4,33 @@
 """The setup script."""
 
 from setuptools import setup, find_packages
+import sys
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+assert sys.version_info >= (3, 6, 0), "black requires Python 3.6+"
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+from pathlib import Path
+
+readme = Path("README.rst").read_text()
+history = Path("HISTORY.rst").read_text()
 
 setup(
-    name='gconfigs',
-    version='0.1.1',
+    name="gconfigs",
+    version="0.1.1",
     description="gConfigs - Config and Secret parser for your Python projects.",
-    long_description=readme + '\n\n' + history,
+    long_description=f"{readme}\n\n{history}",
     author="Douglas Miranda",
-    author_email='douglasmirandasilva@gmail.com',
-    url='https://github.com/douglasmiranda/gconfigs',
-    packages=find_packages(include=['gconfigs']),
+    author_email="douglasmirandasilva@gmail.com",
+    url="https://github.com/douglasmiranda/gconfigs",
+    packages=find_packages(include=["gconfigs"]),
     include_package_data=True,
     license="MIT license",
     zip_safe=False,
-    keywords='gconfigs configs environment secrets dotenv',
+    keywords="gconfigs configs environment secrets dotenv",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
     ],
 )
